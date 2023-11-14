2bbc079280822afc61b28c0cfdbf4bfc5240f382
Tim Cutts <tim.cutts@astrazeneca.com>
2023-11-14 16:26:45 +0000

    Working version, single service

--- lib/r-shiny-on-aws-stack.ts
+++ lib/r-shiny-on-aws-stack.ts
@@ -1,16 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 
 export class RShinyOnAwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    this.templateOptions.description = 'R Shiny on AWS';
 
-    // The code that defines your stack goes here
+    const vpc = new cdk.aws_ec2.Vpc(this, 'RShinyOnAwsVpc', {
+      maxAzs: 3,
+      natGateways: 1,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'RShinyOnAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    const cluster = new ecs.Cluster(this, 'RShinyOnAwsCluster', {
+      vpc
+    });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'RShinyOnAwsTask');
+    taskDefinition.addContainer('RShinyOnAwsContainer', {
+      image: ecs.ContainerImage.fromAsset('containers/shinyapp'),
+      portMappings: [{ containerPort: 3838 }]
+    });
+
+    const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'RShinyOnAwsService', {
+      cluster,
+      taskDefinition,
+      publicLoadBalancer: true,
+    });
+
+    const scaling = service.service.autoScaleTaskCount({ maxCapacity: 10, minCapacity: 1 });
+    scaling.scaleOnCpuUtilization('autoscale_cpu', {
+      targetUtilizationPercent: 50,
+      scaleInCooldown: cdk.Duration.minutes(2),
+      scaleOutCooldown: cdk.Duration.seconds(30)
+    });
+  } 
 }
