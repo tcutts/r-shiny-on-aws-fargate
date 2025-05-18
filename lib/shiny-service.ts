31732ca1dac0b2d9c3b53cf71a29771c17e05ee0
Tim Cutts <tim@thecutts.org>
2025-05-18 07:45:13 +0100

    Health check was failing due to bad routing in containers

--- lib/shiny-service.ts
+++ lib/shiny-service.ts
@@ -12,12 +12,12 @@ export interface ShinyTaskDefinition {
    * The Fargate task definition for the Shiny application.
    */
   taskDefinition: ecs.FargateTaskDefinition;
-  
+
   /**
    * The URL path pattern to route to this Shiny application (e.g., "/app1/*").
    */
   pathPattern: string;
-  
+
   /**
    * The priority of this path pattern in the ALB listener rules.
    * Higher numbers indicate higher priority.
@@ -33,17 +33,17 @@ export interface ShinyServiceProps {
    * The VPC where the Shiny services will be deployed.
    */
   vpc: ec2.Vpc;
-  
+
   /**
    * The ECS cluster where the Fargate tasks will run.
    */
   cluster: ecs.Cluster;
-  
+
   /**
    * List of Shiny application task definitions to deploy.
    */
   taskDefinitions: ShinyTaskDefinition[];
-  
+
   /**
    * CIDR range that is allowed to access the load balancer.
    */
@@ -52,13 +52,13 @@ export interface ShinyServiceProps {
 
 /**
  * A construct that deploys multiple R Shiny applications on AWS Fargate with an Application Load Balancer.
- * 
+ *
  * This construct creates:
  * - An Application Load Balancer to route traffic to different Shiny applications
  * - Fargate services for each Shiny application
  * - Path-based routing rules to direct traffic to the appropriate application
  * - Auto-scaling configuration for each service based on CPU utilization
- * 
+ *
  * Each Shiny application is deployed as a separate Fargate service and is accessible
  * via a unique path pattern on the load balancer.
  */
@@ -67,12 +67,12 @@ export class ShinyService extends Construct {
    * The Application Load Balancer that routes traffic to the Shiny applications.
    */
   public readonly loadBalancer: elasticloadbalancingv2.ApplicationLoadBalancer;
-  
+
   /**
    * The list of Fargate services created for each Shiny application.
    */
   public readonly services: ecs.FargateService[] = [];
-  
+
   /**
    * CloudFormation output containing the URL of the load balancer.
    */
@@ -80,7 +80,7 @@ export class ShinyService extends Construct {
 
   /**
    * Creates a new ShinyService construct.
-   * 
+   *
    * @param scope The parent construct
    * @param id The construct ID
    * @param props Configuration properties for the ShinyService
@@ -162,11 +162,11 @@ export class ShinyService extends Construct {
 
       this.services.push(service);
     });
-    
+
     // Create an output with the load balancer URL
-    this.loadBalancerUrl = new cdk.CfnOutput(this, 'LoadBalancerUrl', {
+    this.loadBalancerUrl = new cdk.CfnOutput(this, "LoadBalancerUrl", {
       value: `http://${this.loadBalancer.loadBalancerDnsName}`,
-      description: 'The URL of the load balancer',
+      description: "The URL of the load balancer",
       exportName: `${id}-LoadBalancerUrl`,
     });
   }
